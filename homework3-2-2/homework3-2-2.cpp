200be8c8afadc90b6116556da4efb85971cdca78
Abrroo <akhmetov.ersyn@gmail.com>
2022-07-07 16:38:53 +0300

    gg

--- homework3-2-2/homework3-2-2.cpp
+++ homework3-2-2/homework3-2-2.cpp
@@ -1,6 +1,36 @@
 ﻿#include <iostream>
+#include <cstring>
+
+struct bankDB
+{
+    unsigned int bankAcc = 0;
+    std::string fName;
+    double bankStatement = 0;
+};
+
+void makeSomeMoney(bankDB* client, double newBankStatement)
+{
+    client->bankStatement = newBankStatement;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    setlocale(LC_ALL, "Russian");
+
+    bankDB client;
+    double newBankStatement = 0;
+
+    std::cout << "Введите номер счёта: ";
+    std::cin >> client.bankAcc;
+    std::cout << "Введите имя владельца: ";
+    std::cin >> client.fName;
+    std::cout << "Введите баланс: ";
+    std::cin >> client.bankStatement;
+    std::cout << "Введите новый баланс: ";
+    std::cin >> newBankStatement;
+
+    makeSomeMoney(&client, newBankStatement);
+
+    std::cout << "Ваш счёт: " << client.fName << ", " << client.bankAcc << ", " << newBankStatement << std::endl;
+    return 0;
 }
